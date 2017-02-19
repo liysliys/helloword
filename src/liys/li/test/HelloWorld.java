d89406462a3dafae01fd6bda81ad6eccc12d6e1b
Administrator <Administrator@SNGRN62J04AQAON>
2017-02-19 11:43:46 +0800

    update02

--- src/liys/li/test/HelloWorld.java
+++ src/liys/li/test/HelloWorld.java
@@ -13,5 +13,8 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		System.out.println("Hello World");
+		for(int i = 0; i <= 10; i++) {
+			System.out.println(i);
+		}
 	}
 }
