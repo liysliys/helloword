a5eac19a459fe702a5ad85173b2d57410ce25aed
liysliys <liyongshun_shun@sina.com>
2017-02-19 14:29:11 +0800

    update01

---
+++ src/liys/li/test/HelloWorld02.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package liys.li.test;
+
+/**
+ * @author liys
+ *
+ */
+public class HelloWorld02 {
+	public static void main(String[] args) {
+		System.out.println("1111111111111111");
+	}
+}
