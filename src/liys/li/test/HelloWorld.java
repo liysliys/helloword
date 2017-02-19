d068c2efe11f0c93da07c2c5a5f1ab9bb4c84dae
Administrator <Administrator@SNGRN62J04AQAON>
2017-02-19 11:05:20 +0800

    update01

---
+++ src/liys/li/test/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package liys.li.test;
+
+/**
+ * @author liys
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
