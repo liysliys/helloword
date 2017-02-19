95426c3b6dc0de7208112883d7be8349227ca297
liysliys <liyongshun_shun@126.com>
2017-02-19 11:44:19 +0800

    Update HelloWorld.java

--- src/liys/li/test/HelloWorld.java
+++ src/liys/li/test/HelloWorld.java
@@ -10,5 +10,8 @@ package liys.li.test;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 }
